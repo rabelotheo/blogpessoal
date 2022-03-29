326977bf08d2676541343b1cca92d4e8bdb4722e
Matheus <matheusrabelosantos@hotmail.com>
2022-03-29 20:56:14 -0300

    Adicionado tb_temas no Blog Pessoal

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,9 +1,12 @@
 package com.generation.blogpessoal.controller;
 
-
 import java.util.List;
 
 import javax.validation.Valid;
+
+import com.generation.blogpessoal.model.Postagem;
+import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,9 +21,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.generation.blogpessoal.model.Postagem;
-import com.generation.blogpessoal.repository.PostagemRepository;
-
 @RestController
 @RequestMapping("/postagens")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -28,6 +28,9 @@ public class PostagemController {
 	
 	@Autowired
 	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository;
 	
 	@GetMapping
 	private ResponseEntity<List<Postagem>> getAll(){
@@ -47,28 +50,33 @@ public class PostagemController {
 	}
 	
 	@PostMapping
-	private ResponseEntity<Postagem> postPostagem(@Valid @RequestBody Postagem postagem){
-		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
-		
+	public ResponseEntity<Postagem> postPostagem (@Valid @RequestBody Postagem postagem){
+		if (temaRepository.existsById(postagem.getTema().getId()))
+			return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
 	
 	@PutMapping
-	private ResponseEntity<Postagem> putPostagem(@Valid @RequestBody Postagem postagem){
-		return postagemRepository.findById(postagem.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem)))
-				.orElse(ResponseEntity.notFound().build());
-}				
+	public ResponseEntity<Postagem> putPostagem (@Valid @RequestBody Postagem postagem){
+		if (postagemRepository.existsById(postagem.getId())){		
+			if (temaRepository.existsById(postagem.getTema().getId()))
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem));		
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();			
+		}			
+			
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}			
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Object> deletePostagem(@PathVariable Long id) {	
+	public ResponseEntity<?> deletePostagem(@PathVariable Long id) {	
 		return postagemRepository.findById(id)
-				.map(resposta -> {postagemRepository.deleteById(id);
+				.map(resposta -> {
+					postagemRepository.deleteById(id);
 					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 				})
 				.orElse(ResponseEntity.notFound().build());
-		
 	}
 	
-	
-	
+		
 }
--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -6,10 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
@@ -31,6 +35,10 @@ public class Postagem {
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public Long getId() {
 		return id;
@@ -62,6 +70,13 @@ public class Postagem {
 
 	public void setData(LocalDateTime data) {
 		this.data = data;
+	}
+	public Tema getTema() {
+		return this.tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 
 }
